1a70be5150bc85686c63e5802609a7ceac373f0d
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-02-15 10:25:01 -0500

    Fase 1.

--- src/Tests/TestEliminarArchivos.java
+++ src/Tests/TestEliminarArchivos.java
@@ -4,7 +4,7 @@ import Logic.EliminarArchivos.EliminarArchivos;
 
 import java.io.FileNotFoundException;
 
-public class PruebasDos {
+public class TestEliminarArchivos {
     public static void main(String[] args) throws FileNotFoundException {
         String ruta = "/home/angel/Documentos/ESCUELA/FASE 2/Master File Manager/src/Laboratorio/SalaDos";
 
